c41724e787def0776252ca0b937f4b9373093df4
Amal Nanavati <amaln@uw.edu>
2020-07-15 20:31:22 -0700

    updated signatures and added Google auto downloader

---
+++ getSignatures.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import pickle
+import os.path
+import io
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def getSignatures():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    file_id = '1l8Uzo5nw7tQgg2AbGL_QS-_uYz2GQcU34cFLjgqSFDw'
+    request = drive_service.files().export_media(fileId=file_id,
+                                                 mimeType='text/csv')
+    fh = open("FUSD Anti-Racism Petition (Responses) - Form Responses 1.csv", "wb")
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print(status)
+        print("Download %d%%." % int(status.progress() * 100))
+
+if __name__ == '__main__':
+    getSignatures()
